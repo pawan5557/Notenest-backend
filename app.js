66083ecd4223a5e09d70806a725fba6c34ac7873
pawan5557 <keswanipawan68@gmail.com>
2026-03-12 17:34:31 +0530

    Refactor : Updated the cors policy

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ const app=express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+app.use(cors({
+  origin: [
+    "http://localhost:3000", 
+    "https://notenestnote.netlify.app/" // Replace this when you get your real link!
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"]
+}));
 import { router_variable } from "./srcbackend/Routes/user.router.js";
 app.use("/users",router_variable)
 
